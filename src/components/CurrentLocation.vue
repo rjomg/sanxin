bfd94b9dd67cb3020f3a3442ab39a57ba19717c5
weiki <wen18826478504@163.com>
2019-04-03 11:06:45 +0800

    当前位置

--- src/components/CurrentLocation.vue
+++ src/components/CurrentLocation.vue
@@ -22,8 +22,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .title{height: 1.5rem; width: 100%; line-height: 20px;}
+  .title{height: 1.5rem; width: 100%; line-height: 20px; margin: 20px 0 20px 0;}
   .line-block{height:20px; width:5px;; background-color:rgb(103,186,247); float: left; margin-right: 0.5rem;}
   .current{float:left; font-weight: 600;}
-  .current-color{float: left; color:#2894FF}
+  .current-color{float: left; font-weight: 500; color:rgb(54, 121, 254)}
 </style>
